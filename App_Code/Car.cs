c6d7c92a9e01078e6260e9dceb7d2d425f1dbb70
hectolec <hectortoran@gmail.com>
2014-12-10 16:55:09 +0100

    Subida 2

---
+++ App_Code/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for CartItem
+/// </summary>
+public class Car{
+    public string matricula {get; set;}
+    public string color {get;set;}
+    public string modelo {get;set;}
+}
---
+++ App_Code/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for CartItem
+/// </summary>
+public class Persona{
+    public string id{get; set;}
+    public string dni{get; set;}
+    public string nom {get;set;}
+    public string img {get;set;}
+    public string telefon {get;set;}
+    public string carrer {get;set;}
+    public string poblacio {get;set;}
+}
